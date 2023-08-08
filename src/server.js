fda57a2d35933ccd6205fd3d349e2e0a46d0fe98
Le Hoang Loi <nhoxac94@yahoo.com>
2023-08-08 21:55:55 +0700

    config view engine

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
+
+const app = express();
+
+const port = 8080;
+
+configViewEngine(app);
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.listen(port, () => {
+  console.log('Server run at ' + port);
+});
